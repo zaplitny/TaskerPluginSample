41b93e4e1519f8488cb9c43666c3e485c3e86aec
Sergei Zaplitny <zaplitny@gmail.com>
2025-03-13 13:25:42 +0100

    Update ActionReceivers.kt

--- taskerpluginlibrary/src/main/java/com/joaomgcd/taskerpluginlibrary/action/ActionReceivers.kt
+++ taskerpluginlibrary/src/main/java/com/joaomgcd/taskerpluginlibrary/action/ActionReceivers.kt
@@ -11,7 +11,7 @@ import com.joaomgcd.taskerpluginlibrary.runner.IntentServiceParallel
 import net.dinglisch.android.tasker.TaskerPlugin
 
 
-class BroadcastReceiverAction : BroadcastReceiver() {
+open class BroadcastReceiverAction : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         resultCode = TaskerPlugin.Setting.RESULT_CODE_PENDING
         try {
