22ff2fd6400145703c1a4791f616bd6efc9749d2
Sergei Zaplitny <zaplitny@gmail.com>
2025-03-13 12:58:05 +0100

    Update ActionReceivers.kt
    exclude tasker 6.4.15

--- taskerpluginlibrary/src/main/java/com/joaomgcd/taskerpluginlibrary/action/ActionReceivers.kt
+++ taskerpluginlibrary/src/main/java/com/joaomgcd/taskerpluginlibrary/action/ActionReceivers.kt
@@ -3,6 +3,7 @@ package com.joaomgcd.taskerpluginlibrary.action
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import com.joaomgcd.taskerpluginlibrary.extensions.canBindFireService
 import com.joaomgcd.taskerpluginlibrary.extensions.mayNeedToStartForeground
 import com.joaomgcd.taskerpluginlibrary.extensions.runFromTasker
@@ -10,7 +11,7 @@ import com.joaomgcd.taskerpluginlibrary.runner.IntentServiceParallel
 import net.dinglisch.android.tasker.TaskerPlugin
 
 
-open class BroadcastReceiverAction : BroadcastReceiver() {
+class BroadcastReceiverAction : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         resultCode = TaskerPlugin.Setting.RESULT_CODE_PENDING
         try {
@@ -23,11 +24,24 @@ open class BroadcastReceiverAction : BroadcastReceiver() {
 
 class IntentServiceAction : IntentServiceParallel("IntentServiceTaskerAction") {
     override fun onHandleIntent(intent: Intent) {
+        if (getAppVersion(ctx, "net.dinglisch.android.taskerm") == "6.4.15") {
+            return
+        }
+        
         val mayNeedToStartForeground: Boolean = intent.mayNeedToStartForeground
         startForegroundIfNeeded(mayNeedToStartForeground)
         val result = TaskerPluginRunnerAction.runFromIntent(this, intent)
         if (!result.hasStartedForeground) {
             startForegroundIfNeeded(mayNeedToStartForeground)
+        }
+    }
+
+    fun getAppVersion(context: Context, packageName: String): String? {
+        return try {
+            val packageInfo = context.packageManager.getPackageInfo(packageName, 0)
+            packageInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            null // App is not installed
         }
     }
 }
